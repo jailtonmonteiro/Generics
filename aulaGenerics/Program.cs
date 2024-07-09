fe5cf51e37a2fbb10079c7e62bddba6e086ee991
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-09 14:37:35 -0300

    Trabalhando apenas com inteiros.

---
+++ aulaGenerics/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace aulaGenerics{
+    class Program{
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("Quantos valores? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for(int i =0; i < n; i++){
+                Console.Write($"Valor {i}: ");
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
